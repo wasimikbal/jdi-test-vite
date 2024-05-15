69abeb7cad4afa309b873d9682164f78c370674b
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-15 16:56:54 +0500

    html containers fetching

---
+++ items-carousel/src/views.js
@@ -0,0 +1,3 @@
+export const scrollContainer = document.querySelector('.carousel-container')
+export const backBtn = document.getElementById('backbtn')
+export const nextBtn = document.getElementById('nextbtn')
