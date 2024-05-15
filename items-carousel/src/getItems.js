c8d45a6e808124770abcca0d98b5bb743af3106f
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-15 16:54:35 +0500

    items array

---
+++ items-carousel/src/getItems.js
@@ -0,0 +1,8 @@
+
+
+export const items = [
+    { path: '../burgers/model1.glb', conatinerID: 'canvas1' },
+    { path: '../burgers/model2.glb', conatinerID: 'canvas2' },
+    { path: '../burgers/model3.glb', conatinerID: 'canvas3' },
+    { path: '../burgers/model4.glb', conatinerID: 'canvas4' }
+]
