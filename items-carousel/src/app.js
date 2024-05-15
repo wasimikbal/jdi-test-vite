a705a7e9c9b20cf920332b6589f47ad3fd586189
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-15 16:58:34 +0500

    main module

--- items-carousel/src/app.js
+++ items-carousel/src/app.js
@@ -1,3 +1,22 @@
 import * as THREE from 'three'
+import { loadModel } from './modelLoader'
+import { items } from './getItems'
+import { nextBtn, backBtn, scrollContainer } from './views'
 
 
+const itemWidth = 300
+const padding = 10
+
+// backBtn.addEventListener('click', () => {
+//     scrollContainer.style.scrollBehavior = "smooth"
+//     scrollContainer.scrollLeft -= itemWidth + padding
+// })
+
+// nextBtn.addEventListener('click', () => {
+//     scrollContainer.style.scrollBehavior = "smooth"
+//     scrollContainer.scrollLeft += itemWidth + padding
+// })
+
+items.forEach((item)=>{
+    loadModel(item.conatinerID, item.path)
+})
